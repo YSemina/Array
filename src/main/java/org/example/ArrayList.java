70d9860f027e926a2c25d6ba785174a4c945a38d
Test <yliyalamotkina@yandex.ru>
2024-12-03 14:53:36 +0300

    Убрала Comporable<E> в описании ArrayList, отформатировала код.

--- src/main/java/org/example/ArrayList.java
+++ src/main/java/org/example/ArrayList.java
@@ -1,16 +1,22 @@
 package org.example;
+
+import java.util.*;
+
 /**
- *Класс для хранения массива данных.
+ * Класс для хранения массива данных.
+ *
  * @author - Юля Семина
  */
-public class ArrayList<E extends Comparable<E>> {
+public class ArrayList<E> {
     /**
      * Переменная для задания ёмкости массива, если используется конструктор по умолчанию
+     *
      * @see ArrayList#ArrayList()
      */
-    private static final int SIZE=10;
+    private static final int SIZE = 10;
     /**
      * Переменная для создания пустого массива, если в качестве ёмкости был передан 0
+     *
      * @see ArrayList#ArrayList(int capacity)
      * Переменная также используется при удалении массива
      * @see ArrayList#removeAll()
@@ -19,114 +25,122 @@ public class ArrayList<E extends Comparable<E>> {
     /**
      * Массив типа Object
      */
-    private  Object[] array;
+    private Object[] array;
     /**
      * Переменная для задания ёмкости массива
      */
     private int capacity;
     /**
      * Переменная, отвечающая за количество элементов в
+     *
      * @see ArrayList
      */
-    private int size=0;
+    private int size = 0;
 
     /**
      * Конструктор по умолчанию создает пустой массив на 10 элементов
      */
-    public ArrayList(){
-        array=new Object[SIZE];
-        capacity=SIZE;
+    public ArrayList() {
+        array = new Object[SIZE];
+        capacity = SIZE;
     }
+
     /**
-     *Конструктор
-     *@param capacity переменная для задания ёмкости массива
-     *@exception IllegalArgumentException возникает, если переданный параметр - отрицательный
+     * Конструктор
+     *
+     * @param capacity переменная для задания ёмкости массива
+     * @throws IllegalArgumentException возникает, если переданный параметр - отрицательный
      */
-    public  ArrayList (int capacity){
-        if(capacity==0){
-            array=EMPTY_ARRAY;
-            this.capacity=0;
-        }
-        else if (capacity > 0) {
+    public ArrayList(int capacity) {
+        if (capacity == 0) {
+            array = EMPTY_ARRAY;
+            this.capacity = 0;
+        } else if (capacity > 0) {
             this.capacity = capacity;
             array = new Object[capacity];
-        }
-        else{
-            throw new IllegalArgumentException("ArrayList не создан, потому что не может содержать отрицательное значение. Ваше значение: "+ capacity);
+        } else {
+            throw new IllegalArgumentException("ArrayList не создан, потому что не может содержать отрицательное значение. Ваше значение: " + capacity);
         }
     }
+
     /**
-     *@return возвращает количество элементов в
+     * @return возвращает количество элементов в
      * @see ArrayList
      * @see ArrayList#size
      */
-    public int size(){
+    public int size() {
         return size;
     }
+
     /**
-     *@return возвращает ёмкость массива
+     * @return возвращает ёмкость массива
      * @see ArrayList
      * @see ArrayList#capacity
      */
-    public int capacity(){
+    public int capacity() {
         return capacity;
     }
+
     /**
-     *@param index индекс, по которому находится объект
-     *@return возвращает элемент по переданному индексу
+     * @param index индекс, по которому находится объект
+     * @return возвращает элемент по переданному индексу
      */
     @SuppressWarnings("unchecked")
-    public E get(int index){
-        return (E)array[index];
+    public E get(int index) {
+        return (E) array[index];
     }
+
     /**
      * Добавляет объект в конец массива
+     *
      * @param obj объект, который нужно добавить в массив.
-     * Увеличивает емкость массива, если места недостаточно
+     *            Увеличивает емкость массива, если места недостаточно
      * @see ArrayList#resize(int)
      */
-    public void add(E obj){
-        if(size<capacity){
-            array[size++]=obj;
-        }
-        else{
-            resize(capacity*2);
+    public void add(E obj) {
+        if (size < capacity) {
+            array[size++] = obj;
+        } else {
+            resize(capacity * 2);
             add(obj);
         }
     }
+
     /**
      * Добавляет объект по указанному индексу.
      * При некорректной передаче index выводит предупреждающие сообщения и завершает работу.
+     *
      * @param index индекс, по которому нужно добавить объект.
-     * @param obj объект, который нужно добавить.
-     * Увеличивает емкость массива, если места недостаточно
+     * @param obj   объект, который нужно добавить.
+     *              Увеличивает емкость массива, если места недостаточно
      * @see ArrayList#resize(int)
      */
-    public void add(int index,E obj){
-        if(size==capacity){
-            resize(capacity*2);
+    public void add(int index, E obj) {
+        if (size == capacity) {
+            resize(capacity * 2);
         }
-        if(index>=capacity){
-            System.out.println("Нельзя вставить элемент по индексу "+index+" потому что он выходит за рамки размера ArrayList.");
+        if (index >= capacity) {
+            System.out.println("Нельзя вставить элемент по индексу " + index + " потому что он выходит за рамки размера ArrayList.");
             return;
         }
-        if((index!=0)&&(array[index-1]==null)){
-            System.out.println("Нельзя вставить элемент по индексу "+index+" потому что перед ним элемента нет.");
+        if ((index != 0) && (array[index - 1] == null)) {
+            System.out.println("Нельзя вставить элемент по индексу " + index + " потому что перед ним элемента нет.");
             return;
         }
-        if((array[index]==null)||(index==0)){
-            array[size++]=obj;
-        }
-        else{
-            insert(index,obj);
+        if ((array[index] == null) || (index == 0)) {
+            array[size++] = obj;
+        } else {
+            insert(index, obj);
         }
     }
+
     /**
      * Удаляет элемент по указанному индексу.
      * При некорректной передаче index выводит предупреждающие сообщения и завершает работу.
+     *
      * @param index индекс, по которму нужно удалить объект.
      */
-    public void remove(int index){
+    public void remove(int index) {
         if (index >= capacity) {
             System.out.println("Некорректный индекс!");
             return;
@@ -135,116 +149,124 @@ public class ArrayList<E extends Comparable<E>> {
             System.out.println("ArrayList пустой, удалять нечего.");
             return;
         }
-        if(array[index]!=null){
-            Object[] newArray=new Object[capacity];
+        if (array[index] != null) {
+            Object[] newArray = new Object[capacity];
             System.arraycopy(array, 0, newArray, 0, index);
-            System.arraycopy(array, index+1,newArray,index,size-index);
+            System.arraycopy(array, index + 1, newArray, index, size - index);
             --size;
-            array=newArray;
-            System.out.println("Объект по индексу "+index+" успешно удалён.");
-        }
-        else{
-            System.out.println("Элеманта по индексу "+index+" не существует.");
+            array = newArray;
+            System.out.println("Объект по индексу " + index + " успешно удалён.");
+        } else {
+            System.out.println("Элеманта по индексу " + index + " не существует.");
         }
     }
+
     /**
      * После вызова метода, массив содержит ссылку на пустой массив.
      */
-    public  void removeAll(){
-        array=EMPTY_ARRAY;
-        this.capacity=0;
-        size=0;
+    public void removeAll() {
+        array = EMPTY_ARRAY;
+        this.capacity = 0;
+        size = 0;
         System.out.println("ArrayList теперь пуст.");
     }
+
     /**
      * Устанавливает переданное значение по указанному индексу. Если по предыдущему индексу нет элемента, выводит предупреждающее сообщение.
+     *
      * @param index индекс, по которому нужно установить значение.
-     * @param obj объект, которым нужно заменить значение по индексу.
+     * @param obj   объект, которым нужно заменить значение по индексу.
      */
-    public void set(int index, E obj){
-        if (index>=capacity){
-            System.out.println("Элемента с индексом "+index+" не существует!");
+    public void set(int index, E obj) {
+        if (index >= capacity) {
+            System.out.println("Элемента с индексом " + index + " не существует!");
             return;
         }
-        if(size==capacity-1){
-            resize(capacity*2);
+        if (size == capacity - 1) {
+            resize(capacity * 2);
         }
-        if((index==0)||(array[index-1] != null)){
-            if(array[index]==null){
+        if ((index == 0) || (array[index - 1] != null)) {
+            if (array[index] == null) {
                 size++;
             }
-            array[index]=obj;
-        }
-        else{
+            array[index] = obj;
+        } else {
             System.out.println("Невозможно установить значение, потому что предыдущий индекс пуст.");
         }
     }
-    /**
-     * Приватный метод для вставки элемента в середину.
-     * @see ArrayList#add(int, E)
-     * @param index индекс, по которому нужно вставить элемент.
-     * @param obj объект, значение которого нужно установить по указанному индексу.
-     */
-    private void insert(int index,E obj){
-        Object[] newArray=new Object[++capacity];
-        if (index >= 0) {
-            for(int i=0;i<index;++i){
-                newArray[i]=array[i];
-            }
-            for(int i=index+1;i<size;++i){
-                newArray[i]=array[i-1];
-            }
-            newArray[index] = obj;
-            ++size;
-        }
-        array=newArray;
-    }
-    /**
-     * Приватный метод для увеличения емкости.
-     * @param  newSize новый размер массива.
-     * @see ArrayList#add(int, E)
-     * @see ArrayList#set(int, E)
-     */
-    private void resize(int newSize){
-        Object[] newArray=new Object[newSize];
-        if (size >= 0)
-            System.arraycopy(array, 0, newArray, 0, size);
-        array=newArray;
-        capacity=newSize;
-    }
-}
 
-class Quick<E extends Comparable<E>>{
     /**
-     * Быстрая сортировка
-     * @param left левая граница.
-     * @param right правая граница.
-     * @param array массив, который нужно отсортировать.
+     * Сортирует ArrayList, используя компаратор, переданный методу.
+     *
+     * @param c компаратор.
      */
-    private void QuickSort(int left, int right, ArrayList<E> array){
-        if (array.size() == 0 || left >= right) return;
+    public void Sort(Comparator<? super E> c) {
+        QuickSort(0, size - 1, c);
+    }
+
+    /**
+     * Приватная быстрая сортировка, используется методом Sort().
+     *
+     * @param left  левая граница.
+     * @param right правая граница.
+     * @param c     компаратор.
+     * @see ArrayList#Sort(Comparator)
+     */
+    @SuppressWarnings("unchecked")
+    private void QuickSort(int left, int right, Comparator<? super E> c) {
+        if (size == 0 || left >= right) return;
         int middle = left + (right - left) / 2;
-        E line = array.get(middle);
+        E line = (E) array[middle];
         int i = left, j = right;
         while (i <= j) {
-            while ((array.get(i).compareTo(line)<0)) i++;
-            while (array.get(j).compareTo(line)>0) j--;
+            while ((c.compare((E) array[i], line) < 0)) i++;
+            while (c.compare((E) array[j], line) > 0) j--;
             if (i <= j) {
-                E swap = array.get(i);
-                array.set(i,array.get(j));
-                array.set(j,swap);
+                E swap = (E) array[i];
+                array[i] = array[j];
+                array[j] = swap;
                 i++;
                 j--;
             }
         }
-        if (left < j) QuickSort(left, j,array);
-        if (right > i) QuickSort(i, right,array);
+        if (left < j) QuickSort(left, j, c);
+        if (right > i) QuickSort(i, right, c);
     }
+
     /**
-     * Сортирует ArrayList.
-     * @param array массив, который нужно отсортировать.
+     * Приватный метод для вставки элемента в середину.
+     *
+     * @param index индекс, по которому нужно вставить элемент.
+     * @param obj   объект, значение которого нужно установить по указанному индексу.
+     * @see ArrayList#add(int, E)
      */
-    public void Sort(ArrayList<E> array){
-        QuickSort(0,array.size()-1,array);
+    private void insert(int index, E obj) {
+        Object[] newArray = new Object[++capacity];
+        if (index >= 0) {
+            for (int i = 0; i < index; ++i) {
+                newArray[i] = array[i];
+            }
+            for (int i = index + 1; i < size; ++i) {
+                newArray[i] = array[i - 1];
+            }
+            newArray[index] = obj;
+            ++size;
+        }
+        array = newArray;
+    }
+
+    /**
+     * Приватный метод для увеличения емкости.
+     *
+     * @param newSize новый размер массива.
+     * @see ArrayList#add(int, E)
+     * @see ArrayList#set(int, E)
+     */
+    private void resize(int newSize) {
+        Object[] newArray = new Object[newSize];
+        if (size >= 0)
+            System.arraycopy(array, 0, newArray, 0, size);
+        array = newArray;
+        capacity = newSize;
     }
 }
--- src/test/java/org/example/ArrayListTest.java
+++ src/test/java/org/example/ArrayListTest.java
@@ -2,42 +2,49 @@ package org.example;
 
 import org.junit.Test;
 
+import java.util.Comparator;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 public class ArrayListTest {
     /**
      * Попытка создать ArrayList с отрицательным значением ёмкости
      */
     @Test
             (expected = IllegalArgumentException.class)
-    public void initNegative(){
+    public void initNegative() {
         ArrayList<Integer> test = new ArrayList<>(-15);
     }
+
     /**
-     *Создание ArrayList'а без передачи значения для задания ёмкости
+     * Создание ArrayList'а без передачи значения для задания ёмкости
      */
     @Test
     public void initEmpty() {
         ArrayList<Integer> test = new ArrayList<>();
-        assertEquals(0,test.size());
+        assertEquals(0, test.size());
     }
+
     /**
-     *Создание ArrayList'а с заданной ёмкостью 0
+     * Создание ArrayList'а с заданной ёмкостью 0
      */
     @Test
     public void initZero() {
         ArrayList<Integer> test = new ArrayList<>(0);
-        assertEquals(0,test.size());
+        assertEquals(0, test.size());
     }
+
     /**
-     *Создание ArrayList'а с заданной положительной ёмкостью
+     * Создание ArrayList'а с заданной положительной ёмкостью
      */
     @Test
     public void initPositive() {
         ArrayList<Integer> test = new ArrayList<>(100);
-        assertEquals(100,test.capacity());
+        assertEquals(100, test.capacity());
     }
+
     /**
-     *Добавление 1000 элементов по порядку при помощи метода add() и передачи ему объекта
+     * Добавление 1000 элементов по порядку при помощи метода add() и передачи ему объекта
      */
     @Test
     public void add1000() {
@@ -45,140 +52,160 @@ public class ArrayListTest {
         for (int i = 0; i < 1000; ++i) {
             test.add(i + 2);
         }
-        assertEquals(1000,test.size());
+        assertEquals(1000, test.size());
     }
+
     /**
-     *Добавление 1000 элементов по порядку при помощи метода add() и передачи ему индекса, по которому нужно установить значение
+     * Добавление 1000 элементов по порядку при помощи метода add() и передачи ему индекса, по которому нужно установить значение
      * и объекта, а также вставка элемента в середину ArrayList'а
      */
     @Test
     public void addByIndex() {
         ArrayList<Integer> test = new ArrayList<>();
         for (int i = 0; i < 1000; ++i) {
-            test.add(i,i + 2);
+            test.add(i, i + 2);
         }
-        test.add(10,1);
-        assertEquals(1001,test.size());
+        test.add(10, 1);
+        assertEquals(1001, test.size());
     }
+
     /**
-     *Попытка добавить элемент по индексу, который выходит за пределы ArrayList'а
+     * Попытка добавить элемент по индексу, который выходит за пределы ArrayList'а
      */
     @Test
     public void addOutsideTheIndex() {
         ArrayList<Integer> test = new ArrayList<>(200);
-        test.add(200,15);
-        for(int i=0;i<200;++i){
-            test.add(i+1);
+        test.add(200, 15);
+        for (int i = 0; i < 200; ++i) {
+            test.add(i + 1);
         }
-        assertEquals(200,test.size());
+        assertEquals(200, test.size());
     }
+
     /**
-     *Метод не вставляет элемент по индексу, если значение перед ним не заполнено
+     * Метод не вставляет элемент по индексу, если значение перед ним не заполнено
      */
     @Test
     public void notAdd() {
         ArrayList<Integer> test = new ArrayList<>(200);
-        test.add(15,222);
-        assertEquals(0,test.size());
+        test.add(15, 222);
+        assertEquals(0, test.size());
     }
+
     /**
-     *Удаление элементов по индексу
+     * Удаление элементов по индексу
      */
     @Test
     public void remove() {
         ArrayList<Integer> test = new ArrayList<>();
         for (int i = 0; i < 1000; ++i) {
-            test.add(i,i + 2);
+            test.add(i, i + 2);
         }
-        for (int i = 999; i >0; --i) {
+        for (int i = 999; i > 0; --i) {
             test.remove(i);
         }
         test.remove(2000);
-        assertEquals(1,test.size());
+        assertEquals(1, test.size());
     }
+
     /**
-     *Удаление ArrayList'а полностью
+     * Удаление ArrayList'а полностью
      */
     @Test
     public void removeAll() {
         ArrayList<Integer> test = new ArrayList<>();
         for (int i = 0; i < 1000; ++i) {
-            test.add(i,i + 2);
+            test.add(i, i + 2);
         }
         test.removeAll();
-        assertEquals(0,test.size());
+        assertEquals(0, test.size());
     }
+
     /**
-     *Установка значения по индексу по порядку
+     * Установка значения по индексу по порядку
      */
     @Test
     public void set1000() {
         ArrayList<Integer> test = new ArrayList<>();
         for (int i = 0; i < 1000; ++i) {
-            test.set(i,i + 2);
+            test.set(i, i + 2);
         }
-        assertEquals(1000,test.size());
+        assertEquals(1000, test.size());
     }
+
     /**
-     *Правильность получения элемента по индексу
+     * Правильность получения элемента по индексу
      */
     @Test
     public void get() {
         ArrayList<Integer> test = new ArrayList<>();
         for (int i = 0; i < 500; ++i) {
-            test.set(i,i + 2);
+            test.set(i, i + 2);
         }
-        test.set(15,222);
-        assertEquals(222,test.get(15));
+        test.set(15, 222);
+        assertEquals(222, test.get(15));
     }
+
     /**
-     *Установка значений по индексу
+     * Установка значений по индексу
      */
     @Test
     public void set() {
         ArrayList<Integer> test = new ArrayList<>();
-        int ch=0;
+        int ch = 0;
         for (int i = 0; i < 500; ++i) {
-            test.set(i,1);
-            ch+=test.get(i);
+            test.set(i, 1);
+            ch += test.get(i);
         }
-        assertEquals(500,ch);
+        assertEquals(500, ch);
     }
+
     /**
-     *Корректность возврвта размера ArrayList'а
+     * Корректность возврвта размера ArrayList'а
      */
     @Test
     public void size() {
         ArrayList<Integer> test = new ArrayList<>(200);
         for (int i = 0; i < 200; ++i) {
-            test.set(i,i + 2);
+            test.set(i, i + 2);
         }
-        assertEquals(200,test.size());
+        assertEquals(200, test.size());
     }
+
     /**
-     *Корректность работы сортировки
+     * Корректность работы сортировки
      */
     @Test
-    public void Sort(){
-        Quick<Integer> testSort=new Quick<>();
+    public void Sort() {
+        Comparator<Integer> comp = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        };
         ArrayList<Integer> test = new ArrayList<>();
         for (int i = 0; i < 1000; ++i) {
             test.add(i + 2);
         }
-        test.set(10,10000);
-        test.set(900,15);
-        testSort.Sort(test);
-        assertTrue(test.get(10)<test.get(900));
-    }
-    /**
-     *Корректность работы сортировки пустого ArrayList'а
-     */
-    @Test
-    public void EmptySort(){
-        Quick<Integer> testSort=new Quick<>();
-        ArrayList<Integer> test = new ArrayList<>(0);
-        testSort.Sort(test);
-        assertEquals(0,test.size());
+        test.set(10, 10000);
+        test.set(900, 15);
+        test.Sort(comp);
+        assertTrue(test.get(10) < test.get(900));
     }
 
+    /**
+     * Корректность работы сортировки пустого ArrayList'а
+     */
+    @Test
+    public void EmptySort() {
+        Comparator<Integer> comp = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+        };
+        ArrayList<Integer> test = new ArrayList<>(0);
+        test.Sort(comp);
+        assertEquals(0, test.size());
+    }
 }
